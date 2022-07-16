b720b58b93a0829d9b8fc00ef951ea1dcb514727
Chandler Miller <chandlerbing98@gmail.com>
2022-07-16 15:49:15 -0600

    updated interfaces

--- interfaces/std_interface.go
+++ interfaces/std_interface.go
@@ -3,9 +3,32 @@ package interfaces
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/rs/zerolog"
 )
+
+// Set global log value
+func SetGlobalLevel(level string) {
+	input := strings.ToLower(level)
+	switch input {
+	case "panic":
+		zerolog.SetGlobalLevel(zerolog.PanicLevel)
+	case "fatal":
+		zerolog.SetGlobalLevel(zerolog.FatalLevel)
+	case "error":
+		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+	case "warn":
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	case "info":
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	case "debug":
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	case "trace":
+		zerolog.SetGlobalLevel(zerolog.TraceLevel)
+	}
+
+}
 
 // stores messages to log later, from standard interface
 type Event struct {
@@ -47,4 +70,8 @@ func (l *StandardLogger) InvalidArgValue(argumentName string, T any) {
 // standard error message
 func (l *StandardLogger) MissingArg(argumentName string) {
 	l.logger.Error().Msg((missingArgMessage.message + " " + argumentName))
+}
+
+func (l *StandardLogger) Std(T any) {
+	l.logger.Info().Stack().Msg(fmt.Sprint(T))
 }
