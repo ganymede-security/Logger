694934d68e1594b0bea29ae6f6be9133a0afbbd3
Chandler Miller <chandlerbing98@gmail.com>
2022-08-02 08:01:18 -0600

    vue setup

--- logger/src/App.vue
+++ logger/src/App.vue
@@ -1,31 +1,97 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+
+var d = new Date()
+let hr: number; hr = d.getUTCHours();console.log(hr)
+let min: number; min = d.getUTCMinutes();console.log(min)
+
+
+
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <header>
+      <h2>Home</h2>
+    </header>
+    <fieldset>
+        <main>
+            <div class="sidenav">
+                <a href="#">Average End-to-End Latency </a>
+                <a href="#">Average Response Time </a>
+                <a href="#">Errors Per Minute </a>
+                <a href="#">Exceptions Per Minute </a>
+            </div>
+        </main>
+        <div id="app">
+          <h2 class="summary-title">Summary</h2>
+          <div class="error"></div>
+          <p class="error-text">Error</p>
+          <div class="info"></div>
+          <p class="info-text">Info</p>
+          <canvas ref="log-canvas" width="550" height="450"></canvas>
+        </div>
+    </fieldset>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+
+.sidenav {
+  height: 100%;
+  width: 320px;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: #121212;
+  overflow-x: hidden;
+  padding-top: 20px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.sidenav a {
+  padding: 6px 8px 6px 16px;
+  text-decoration: none;
+  font-size: 22px;
+  color: #818181;
+  display: block;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.error {
+  height: 10px;
+  width: 10px;
+  background-color: #c91c45;
+  border-radius: 50%;
+  display: inline-block;
+  outline-color: black;
+  outline-style: solid;
+  outline-width: thin;
 }
+
+.info {
+  height: 10px;
+  width: 10px;
+  background-color: #4735b8;
+  border-radius: 50%;
+  display: inline-block;
+  outline-color: black;
+  outline-style: solid;
+  outline-width: thin;
+}
+
+.info-text{
+  display:inline-block;
+  padding-left: 10px;
+}
+
+.error-text{
+  display:inline-block;
+  padding-left: 10px;
+  padding-right: 10px;
+}
+
+.summary-title{
+  color: white;
+  -webkit-text-stroke: 1px;
+  -webkit-text-stroke-color: black;
+}
+
 </style>
